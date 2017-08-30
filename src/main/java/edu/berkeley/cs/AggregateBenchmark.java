0f4ea0fa590692bd6b1ed2097db52cf5f547cb9e
Anurag Khandelwal <anuragk@berkeley.edu>
2017-08-29 20:54:38 -0700

    Debug

--- src/main/java/edu/berkeley/cs/AggregateBenchmark.java
+++ src/main/java/edu/berkeley/cs/AggregateBenchmark.java
@@ -28,6 +28,8 @@ class AggregateBenchmark extends JDBCBenchmark {
                 try {
                     ResultSet rs = statement.executeQuery();
                     while(rs.next()) {
+                        System.out.println(rs.getString(1) + ", " + rs.getString(2)
+                                + ", " + rs.getString(3) + ", " + rs.getString(4));
                         count++;
                     }
                 } catch (SQLException e) {
